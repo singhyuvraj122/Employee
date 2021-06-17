839f0a167355aee3b391b6f01ad8d5ce0d889f42
YUVRAJ SINGH <singhyuvraj122@gmail.com>
2021-06-18 05:19:12 +0530

    completed ondelete php page

--- OpDelete.php
+++ OpDelete.php
@@ -1,6 +1,33 @@
-<?
+<?php
+
+include "config.php";
+
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["email"];
 
 var_dump($_POST);
+
+$qry = "select * from empdata where empFname = '$fname' AND emplname = '$lname' AND empEmail = '$email'";
+
+$res = mysqli_query($conn,$qry);
+
+var_dump($res);
+
+if($res->lengths > 0){
+  $qry = "DELETE FROM empdata WHERE empFname = '$fname' AND emplname = '$lname' AND empEmail = '$email'";
+  $res = mysqli_query($conn,$qry);
+  
+  if($res){
+    echo("query done");
+  }
+  else{
+    echo("query not done");
+  }
+}
+else{
+  echo("field not found");
+}
 
 
 ?>
