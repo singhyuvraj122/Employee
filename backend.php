56d2f85065569acceb8ee1142b782fdd190696a0
YUVRAJ SINGH <singhyuvraj122@gmail.com>
2021-06-18 00:04:29 +0530

    added database connectivity

--- backend.php
+++ backend.php
@@ -1,21 +1,39 @@
 <?php
 
+use function PHPSTORM_META\type;
+
 include "config.php";
 
 $fname = $_POST["fname"];
 $lname = $_POST["lname"];
-$age = $_POST["age"];
+$age = (int)$_POST["age"];
 $gender = $_POST["gender"];
 $city = $_POST["city"];
 $state = $_POST["state"];
 $country = $_POST["country"];
 $email = $_POST["email"];
-$phone = $_POST["phone"];
+$phone = (int)$_POST["phone"];
 
 echo("<h1>backend page</h1>");
 var_dump($_POST);
 echo("my name is {$fname} {$lname}<br>");
 echo("i am {$age} years old and {$gender}<br>");
 echo("i live in {$city} {$state} ${country}<br>");
-echo("my email is {$email} and phone numebr is {$phone}<br>");
+echo("my email is $email and phone numebr is $phone<br>");
+
+echo(gettype($phone));
+
+$qry = "INSERT INTO empData(empFname) VALUES('ramesh')";
+
+$res = mysqli_query($conn,$qry);
+
+var_dump($res);
+
+if($res){
+  echo("query run");
+}
+else{
+  echo("query not run");
+}
+
 ?>
--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
 
-if(mysqli_connect("localhost","root","kamwalibai","")){
-  echo("<h1>connection established</h1>");
-}
+$conn = mysqli_connect("localhost","root","kamwalibai","employee",3306);
 
+
+if($conn){
+  echo("<h1>connection established</h1>");
+  var_dump($conn);
+}
+else{
+  echo"<h1>connection failed</h1>";
+  var_dump($conn);
+}
 ?>
