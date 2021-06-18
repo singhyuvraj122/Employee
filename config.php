d6bc1160b336fe5e19414385f4ceb62471658f5f
YUVRAJ SINGH <singhyuvraj122@gmail.com>
2021-06-18 12:55:46 +0530

    added disp table format

--- config.php
+++ config.php
@@ -3,10 +3,7 @@
 $conn = mysqli_connect("localhost","root","kamwalibai","employee",3306);
 
 
-if($conn){
-  echo("<h1>connection established</h1>");
-}
-else{
-  echo"<h1>connection failed</h1>";
+if(!$conn){
+  echo("<h1>connection failed</h1>");
 }
 ?>
--- index.php
+++ index.php
@@ -1,46 +1,47 @@
 <?php
 include "config.php";
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
   <head>
+    <title>Home</title>
     <link rel="stylesheet" href="st.css">
   </head>
   <body>
     <div id="outerCont">
-      <div class="contItem" id="innerCont1">
-        <h1>Employee Admin Dashboard</h1>
-      </div>
       <div class="contItem" id="innerCont2">
-        <li><a href="OpInsert.html">Insert Employee</a></li>
-        <li><a href="OpUpdate.html">Update Employee</a></li>
-        <li><a href="OpDelete.html">Delete Employee</a></li>
+        <div id="navTitle">
+          Employee Dashboard
+        </div>
+        <div id="navbar">
+          <li><a href="OpInsert.html">Insert Employee</a></>
+          <li><a href="OpUpdate.html">Update Employee</a></li>
+          <li><a href="OpDelete.html">Delete Employee</a></li>
+        </div>
       </div>
       <div class="contItem" id="innerCont3">
-        <?php
-        $qry = "SELECT * FROM empdata";
-
-        $res = mysqli_query($conn,$qry);
-
-        // var_dump($res);
-        if($res->num_rows > 0){
-          while($row = mysqli_fetch_assoc($res)) {
+      <table>
+            <tr>
+                <th>GFG UserHandle</th>
+                <th>Practice Problems</th>
+                <th>Coding Score</th>
+                <th>GFG Articles</th>
+            </tr>
+            <?php   // LOOP TILL END OF DATA 
+                while($rows=$result->fetch_assoc())
+                {
             ?>
-            <table>
-              <tr>
-                <td><?php echo $row['empFname']; ?></td>
-                <td><?php echo $row['emplname']; ?></td>
-                <td><?php echo $row['empAge']; ?></td>
-                <td><?php echo $row['empGender']; ?></td>
-                <td><?php echo $row['empCity']; ?></td>
-                <td><?php echo $row['empState']; ?></td>
-                <td><?php echo $row['empCountry']; ?></td>
-                <td><?php echo $row['empEmail']; ?></td>
-                <td><?php echo $row['empPhone']; ?></td>
-              </tr>
-            </table>
+            <tr>
+                <td><?php echo $rows['empFname'];?></td>
+                <td><?php echo $rows['problems'];?></td>
+                <td><?php echo $rows['score'];?></td>
+                <td><?php echo $rows['articles'];?></td>
+            </tr>
             <?php
-          }}?>
+                }
+            ?>
+        </table>
+
       </div>
     </div>
     <script src="st.js"></script>
