1dd2c0c0bad0f61cd7a87f320de1bbd22f709d69
YUVRAJ SINGH <singhyuvraj122@gmail.com>
2021-06-19 15:52:12 +0530

    added new table

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include "config.php";
+$qry = "SELECT * FROM empdata";
+$res = mysqli_query($conn,$qry);
+?>
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -6,7 +11,7 @@
   </head>
   <body>
     <div id="outerCont">
-      <div class="contItem" id="innerCont2">
+      <div id="innerCont2">
         <div id="navTitle">
           Employee Dashboard
         </div>
@@ -16,19 +21,42 @@
           <li><a href="OpDelete.html">Delete Employee</a></li>
         </div>
       </div>
-      <div class="contItem" id="innerCont3">
+      <div id="innerCont3">
         <table>
-          <tr>
-            <th>NAME</th>
-            <th>SURNAME</th>
-            <th>AGE</th>
-            <th>GENDER</th>
-            <th>CITY</th>
-            <th>STATE</th>
-            <th>COUNTRY</th>
-            <th>EMAIL</th>
-            <th>PHONE</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>INDEX</th>
+              <th>NAME</th>
+              <th>SURNAME</th>
+              <th>AGE</th>
+              <th>GENDER</th>
+              <th>CITY</th>
+              <th>STATE</th>
+              <th>COUNTRY</th>
+              <th>EMAIL</th>
+              <th>PHONE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php 
+            $i = 1;
+            while(($row = mysqli_fetch_assoc($res)) && ($i <= $res->num_rows)) {
+              echo"<tr>";
+              echo "<td>$i</td>";
+              echo "<td>{$row['empFname']}</td>";
+              echo "<td>{$row['emplname']}</td>";
+              echo "<td>{$row['empAge']}</td>";
+              echo "<td>{$row['empGender']}</td>";
+              echo "<td>{$row['empCity']}</td>";
+              echo "<td>{$row['empState']}</td>";
+              echo "<td>{$row['empCountry']}</td>";
+              echo "<td>{$row['empEmail']}</td>";
+              echo "<td>{$row['empPhone']}</td>";
+              echo "</tr>";
+              $i++;
+            }
+            ?>
+          </tbody>
         </table>
       </div>
     </div>
